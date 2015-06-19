da58bb3f1e1c5ab1dcca2d98c2014ba361e334ab
SuperPcf <1459265227@qq.com>
2015-06-19 18:02:36 +0800

    up

---
+++ app/src/main/java/com/school/pcfcar/cartest/SpaceImageDetailActivity.java
@@ -0,0 +1,74 @@
+package com.school.pcfcar.cartest;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.ViewGroup;
+import android.widget.ImageView.ScaleType;
+
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.school.pcfcar.cartest.pcf.uilt.SmoothImageView;
+import android.net.Uri;
+public class SpaceImageDetailActivity extends Activity {
+
+	private String mPath;
+	private int mPosition;
+	private int mLocationX;
+	private int mLocationY;
+	private int mWidth;
+	private int mHeight;
+	SmoothImageView imageView = null;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_main);
+
+        mPath = (String) getIntent().getSerializableExtra("mPath");
+		//mPosition = getIntent().getIntExtra("position", 0);
+		mLocationX = getIntent().getIntExtra("locationX", 0);
+		mLocationY = getIntent().getIntExtra("locationY", 0);
+		mWidth = getIntent().getIntExtra("width", 0)*2;
+		mHeight = getIntent().getIntExtra("height", 0)*2;
+
+
+		imageView = new SmoothImageView(this);
+		imageView.setOriginalInfo(mWidth, mHeight, mLocationX, mLocationY);
+		imageView.transformIn();
+		imageView.setLayoutParams(new ViewGroup.LayoutParams(-1, -1));
+		imageView.setScaleType(ScaleType.FIT_CENTER);
+        setContentView(imageView);
+
+    	ImageLoader.getInstance().displayImage(mPath, imageView);
+
+        //imageView.setImageResource(R.drawable.temp);
+		// ScaleAnimation scaleAnimation = new ScaleAnimation(0.5f, 1.0f, 0.5f,
+		// 1.0f, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF,
+		// 0.5f);
+		// scaleAnimation.setDuration(300);
+		// scaleAnimation.setInterpolator(new AccelerateInterpolator());
+		// imageView.startAnimation(scaleAnimation);
+
+	}
+
+	@Override
+	public void onBackPressed() {
+		imageView.setOnTransformListener(new SmoothImageView.TransformListener() {
+			@Override
+			public void onTransformComplete(int mode) {
+				if (mode == 2) {
+					finish();
+				}
+			}
+		});
+		imageView.transformOut();
+	}
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+		if (isFinishing()) {
+			overridePendingTransition(0, 0);
+		}
+	}
+
+}
